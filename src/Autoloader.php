fba16a422c55cdf4560ff9cf36f70a1c2fdd50ef
TangRufus <tangrufus@gmail.com>
2020-05-18 12:29:55 +0800

    Remove plugin header

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -1,13 +1,4 @@
 <?php
-/**
- * Plugin Name: Bedrock Autoloader
- * Plugin URI: https://github.com/roots/bedrock/
- * Description: An autoloader that enables standard plugins to be required just like must-use plugins. The autoloaded plugins are included during mu-plugin loading. An asterisk (*) next to the name of the plugin designates the plugins that have been autoloaded.
- * Version: 1.0.0
- * Author: Roots
- * Author URI: https://roots.io/
- * License: MIT License
- */
 
 namespace Roots\Bedrock;
 
